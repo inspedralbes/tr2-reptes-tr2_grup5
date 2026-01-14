b06f5588ca37e59a86917219a256130b1b62a7f9
a24llocarfus <a24llocarfus@inspedralbes.cat>
2026-01-14 12:14:17 +0100

    a veure si funciona

--- frontend/src/pages/login.vue
+++ frontend/src/pages/login.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="login-container">
-    <h1>Iniciar Sesión penis</h1>
+    <h1>Iniciar Sesión</h1>
     <form @submit.prevent="handleLogin">
     <div class="form-group">
         <label for="email">Email:</label>
