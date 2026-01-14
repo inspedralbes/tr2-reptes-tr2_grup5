5ac9847e82235210dc1afbb2a943df7d6e5e24aa
Iker Lopez <a24ikelopgom@inspedralbes.cat>
2026-01-14 12:16:16 +0100

    funciona la peticion de talleres

--- backend/src/controllers/centres/professoratController.js
+++ backend/src/controllers/centres/professoratController.js
@@ -21,6 +21,46 @@ const professoratController = {
             console.error(error);
             res.status(500).json({ message: "Error al obtenir els professors del centre." });
         }
+    },
+
+    
+    createProf: async (req, res) => {
+        try {
+            const user_id_admin = req.user.id; 
+            const { nom, cognoms, email } = req.body;
+
+            
+            const centre = await Centre.findByUserId(user_id_admin);
+            if (!centre) {
+                return res.status(404).json({ message: "Centre no trobat." });
+            }
+
+            
+            const result = await Professor.create({ nom, cognoms, email, centre_id: centre.id });
+            res.status(201).json({
+                id: result.professorId, 
+                nom,
+                cognoms,
+                email
+            });
+            res.status(201).json({
+                id: result.professorId, 
+                nom,
+                cognoms,
+                email,
+                centre_id: centre.id
+            });
+        } catch (error) {
+            console.error("Error al controlador professoratController:", error);
+            
+            if (error.code === 'ER_DUP_ENTRY') {
+                return res.status(400).json({ 
+                    message: "Aquest correu electrònic ja està registrat en el sistema." 
+                });
+            }
+
+            res.status(500).json({ message: "Error intern al crear el professor." });
+        }
     }
 };
 
