e9c93ca03fef69aeb0caa8d8dafb976e2044de6f
a24llocarfus <a24llocarfus@inspedralbes.cat>
2026-01-22 08:59:31 +0100

    data

--- database/init.sql
+++ database/init.sql
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS tallers (
     adreca VARCHAR(255),
     ubicacio VARCHAR(255),
     estat_taller ENUM('inscripcio', 'actiu', 'inactiu', 'confirmacio') DEFAULT 'inactiu',
-    actiu TINYINT(1) DEFAULT 1
+    data_execucio DATE(null)
 ) ENGINE=InnoDB;
 
 -- 6. Taula: peticions
